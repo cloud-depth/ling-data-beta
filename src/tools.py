b03a20d502364e3e3ec4aab3ade00c67e54166bd
Admin <55500771+L9qmzn@users.noreply.github.com>
2024-05-09 06:05:30 +0800

    更快的tokenizer，摆脱对tiktoken的依赖

--- src/tools.py
+++ src/tools.py
@@ -1,8 +1,4 @@
 import os
-import sys
-
-project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(project_root)
 
 
 def show_log_base(worker_dict, show_result, proc_name):
@@ -16,24 +12,20 @@ def show_log_base(worker_dict, show_result, proc_name):
 
 class TokenLen:
     def __init__(self, encoding="qwen"):
+        from tokenizers import Tokenizer
         self.encoding = encoding
-        if encoding == "qwen":
-            from tokenizers import Tokenizer
-            self.fast_tokenizer = Tokenizer.from_file("data/source/tokenizer/tokenizer_qwen.json")
-        elif encoding == "gpt":
-            import tiktoken
-            self.enc = tiktoken.get_encoding("cl100k_base")
+        self.project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        tokenizer_path_dict = {
+            "qwen": "data/source/tokenizer/tokenizer_qwen.json",
+            "gpt": "data/source/tokenizer/tokenizer_gpt.json",
+            # 可拓展
+        }
+        tokenizer_path = os.path.join(self.project_root, tokenizer_path_dict.get(self.encoding))
+        self.tokenizer = Tokenizer.from_file(tokenizer_path)
 
     def __call__(self, text):
-        if self.encoding == "qwen":
-            t = self.fast_tokenizer.encode(text)
-            return len(t.ids)
-        elif self.encoding == "gpt":
-            return len(self.enc.encode(text))
-        elif self.encoding == "words":
-            return len(text.split())
-        else:
-            return len(text)
+        t = self.tokenizer.encode(text)
+        return len(t.ids)
 
 
 def draw_len(lengths, title='Distribution of Element Lengths', xlabel='Length of Elements', ylabel='Frequency'):
